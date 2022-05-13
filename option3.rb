df48cdc3ac6a593d8158de75a935ece36198470d
Luis Fernando <lfmnovaes@gmail.com>
2022-05-13 14:54:19 -0300

    Add solution to option3

---
+++ option3.rb
@@ -0,0 +1,22 @@
+##
+# It splits the expression into tokens, and for each token, if it's an operator, it pops the top two
+# elements off the stack, applies the operator to them, and pushes the result back onto the stack;
+# otherwise, it pushes the token onto the stack
+#
+# Args:
+#   expression: a string containing the expression to be evaluated (operators and numbers
+#     separated by white spaces)
+def rpn_calculator(expression)
+  stack = []
+  expression.split.each do |token|
+    case token
+    when '+', '-', '*', '/'
+      op2 = stack.pop
+      op1 = stack.pop
+      stack.push(op1.send(token, op2))
+    else
+      stack.push(token.to_i)
+    end
+  end
+  stack.last
+end
---
+++ test_option3.rb
@@ -0,0 +1,18 @@
+require './option3'
+require 'test/unit/assertions'
+
+include Test::Unit::Assertions
+
+[
+  ['1 2 +', 3],
+  ['1 2 3 + +', 6],
+  ['2 1 2 + *', 6],
+  ['1 2 3 4 + + +', 10],
+  ['3 2 1 + *', 9],
+  ['1 1 + 3 * 2 *', 12],
+  ['3 2 1 + * 5 +', 14],
+  ['3 2 1 + * 5 + 6 +', 20],
+  ['10 21 55 + *', 760],
+].each { |expression, expected| assert_equal(rpn_calculator(expression), expected) }
+
+# If no errors, then all tests passed.
