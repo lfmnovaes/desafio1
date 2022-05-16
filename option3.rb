218879513d76c97c6d871766525e2827d619ef6a
Luis Fernando <lfmnovaes@gmail.com>
2022-05-16 13:41:53 -0300

    Fix code and README

--- option3.rb
+++ option3.rb
@@ -7,17 +7,35 @@
 #   expression: a string containing the expression to be evaluated (operators and numbers
 #     separated by white spaces)
 
-def rpn_calculator(expression)
-  stack = []
-  expression.split.each do |token|
-    case token
-    when '+', '-', '*', '/'
-      op2 = stack.pop
-      op1 = stack.pop
-      stack.push(op1.send(token, op2))
-    else
-      stack.push(token.to_i)
-    end
+class App
+  def run
+    puts 'RPN (Reverse Polish Notation) Calculator'
+    puts 'Enter an expression to be evaluated (operators and numbers separated by white spaces or new line)'
+    puts 'Enter "=" to show the result'
+    input = gets("=\n").chomp
+    expression = input.gsub(%r{[\/\n,=]}, ' ').strip
+    puts "The answer of the expression #{expression} is: #{rpn_calculator(expression)}"
   end
-  stack.last.round(3)
+
+  def rpn_calculator(expression)
+    stack = []
+    expression.split.each do |token|
+      case token
+      when '+', '-', '*', '/'
+        op2 = stack.pop
+        op1 = stack.pop
+        stack.push(op1.send(token, op2))
+      else
+        stack.push(token.to_i)
+      end
+    end
+    stack.last.round(3)
+  end
 end
+
+def main
+  app = App.new
+  app.run
+end
+
+main if __FILE__ == $0
--- test_option3.rb
+++ test_option3.rb
@@ -3,6 +3,8 @@ require 'test/unit/assertions'
 
 include Test::Unit::Assertions
 
+app = App.new
+
 [
   ['1 2 +', 3],
   ['1 2 3 + +', 6],
@@ -13,6 +15,6 @@ include Test::Unit::Assertions
   ['3 2 1 + * 5 +', 14],
   ['3 2 1 + * 5 + 6 +', 20],
   ['10 21 55 + *', 760],
-].each { |expression, expected| assert_equal(rpn_calculator(expression), expected) }
+].each { |expression, expected| assert_equal(app.rpn_calculator(expression), expected) }
 
 # If no errors, then all tests passed.
